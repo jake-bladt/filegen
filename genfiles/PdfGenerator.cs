8635d7ee65d6859385ff8086c19d3895914eedf9
Jake Bladt <jake@jakebladt.com>
2019-05-08 14:38:50 -0400

    +wordsfile

---
+++ genfiles/PdfGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+namespace genfiles
+{
+    public class PdfGenerator
+    {
+
+        public bool GenerateRandomPdfs(string rootDir, long targetBytes, int maxBytesPerFile)
+        {
+
+
+
+            return false;
+        }
+
+        public bool GenerateRandomPdf(int maxBytes)
+        {
+            var doc = new Document(PageSize.LETTER, 10, 10, 10, 10);
+
+        }
+
+    }
+}
