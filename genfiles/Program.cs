621efc3a712e71e0d419e53aa48fa65ab44fea27
Jake Bladt <jake@jakebladt.com>
2019-05-08 14:19:32 -0400

    initial commit

---
+++ genfiles/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace genfiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Usage();
+                return;
+            }
+
+            var rootDir = args[0];
+
+
+            Console.WriteLine("Hello World!");
+        }
+
+
+        public static void Usage()
+        {
+            Console.WriteLine("To launch this script, use the following form:");
+            Console.WriteLine("  genfiles root-directory");
+        }
+
+    }
+}
